68b789dff672dd5f17f10c2aba08a6a35da71a6a
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-20 21:36:17 -0600

    (feat) add footer to DOM; add react-router-dom to node_modules

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import "./Footer.css";
 function Footer(props) {
-  return <footer>TESTING THIS TEXT</footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__creator">Aaron J Meighoo 2024</p>
+      <a className="footer__credits">Credits</a>
+    </footer>
+  );
 }
 
 export default Footer;
