486994fc07f7f350adac8b61f8384c2a694b4944
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-28 12:27:03 -0600

    ...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Preloader from "../Preloader/Preloader";
